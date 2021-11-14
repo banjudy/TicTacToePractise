c96fdec383a40f0e986f1dc83211d64e1defff29
banjudy <banjudy@gmail.com>
2021-11-14 16:04:05 +0100

    Initial commit

---
+++ src/main/java/javawebsocket/TicTacToePractise/TicTacToePractiseApplication.java
@@ -0,0 +1,13 @@
+package javawebsocket.TicTacToePractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToePractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToePractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javawebsocket/TicTacToePractise/TicTacToePractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package javawebsocket.TicTacToePractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToePractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
